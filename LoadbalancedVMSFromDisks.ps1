27e16056b60c577e34644e851eacca01ff547dfe
unknown <Vishwas.Lele@L00617.appliedis.com>
2013-01-27 18:38:58 -0500

    updated the service name

--- LoadbalancedVMSFromDisks.ps1
+++ LoadbalancedVMSFromDisks.ps1
@@ -32,7 +32,7 @@ $instanceSize = 'Small'
 
 
 # Has to be a unique name. Verify with Test-AzureService
-$serviceName = 'VLLBWFE3' 
+$serviceName = 'VLLBWFE1' 
 
 Get-AzureVMImage | where { ($_.Category -eq "Microsoft")   }
 
@@ -42,20 +42,18 @@ $vmname1 = 'WFE1'
 # Server Name
 $vmname2 = 'WFE2'
 
-$WFE1 = New-AzureVMConfig -Name $vmname1 -DiskName 'VLLBWFE3-WFE1-0-20130125220829' -InstanceSize 'Small' -AvailabilitySetName  $avsetwfe |
+$WFE1 = New-AzureVMConfig -Name $vmname1 -DiskName 'VLLBWFE3-WFE2-0-20130125220859' -InstanceSize 'Small' -AvailabilitySetName  $avsetwfe |
 	
-    Add-AzureProvisioningConfig	-Windows -Password "Devise#!!!" |			
+    #Add-AzureProvisioningConfig	-Windows -Password "Devise#!!!" |			
 	Add-AzureEndpoint -Name 'Remote Desktop' -LocalPort 3389 -Protocol tcp |
-    #Add-AzureEndpoint -Name 'http' -LocalPort 80 -Protocol tcp -LBSetName $lbsetwfe -ProbePort 80 -ProbeProtocol http -ProbePath '/heathCheck/check.aspx' -PublicPort 80 
-    Add-AzureEndpoint -Name 'http' -LocalPort 80 -Protocol tcp -LBSetName $lbsetwfe  -PublicPort 80 
+    Add-AzureEndpoint -Name 'http' -LocalPort 80 -Protocol tcp -LBSetName $lbsetwfe -ProbePort 80 -ProbeProtocol http -ProbePath '/heathCheck/check.aspx' -PublicPort 80 
 
-$WFE2 = New-AzureVMConfig -Name $vmname2 -DiskName 'VLLBWFE3-WFE2-0-20130125220857' -InstanceSize 'Small' -AvailabilitySetName  $avsetwfe |
+$WFE2 = New-AzureVMConfig -Name $vmname2 -DiskName 'VLLBWFE3-WFE2-0-20130125220860' -InstanceSize 'Small' -AvailabilitySetName  $avsetwfe |
 	
 
-    Add-AzureProvisioningConfig	-Windows -Password "Devise#!!!" |					
+    #Add-AzureProvisioningConfig	-Windows -Password "Devise#!!!" |					
 	Add-AzureEndpoint -Name 'Remote Desktop' -LocalPort 3389 -Protocol tcp |
-    #Add-AzureEndpoint -Name 'http' -LocalPort 80 -Protocol tcp -LBSetName $lbsetwfe -ProbePort 80 -ProbeProtocol http -ProbePath '/heathCheck/check.aspx' -PublicPort 80 
-    Add-AzureEndpoint -Name 'http' -LocalPort 80 -Protocol tcp -LBSetName $lbsetwfe  -PublicPort 80 
+    Add-AzureEndpoint -Name 'http' -LocalPort 80 -Protocol tcp -LBSetName $lbsetwfe -ProbePort 80 -ProbeProtocol http -ProbePath '/heathCheck/check.aspx' -PublicPort 80 
 
 New-AzureVM -ServiceName $serviceName -Location $location -VMs $WFE1, $WFE2  					
 
